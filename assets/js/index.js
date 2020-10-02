63e34e340bbda997f25f261bfbb291dee7d09c2e
Vincent <22006984+vincentzhangz@users.noreply.github.com>
2020-10-02 08:05:39 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 600
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 100,
+                "size_min": 0.1,
+                "sync": true
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": true,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+
+function openLink(url) {
+    window.open(url)
+}
